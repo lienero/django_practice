015cccca8b0c99588a0836f8b8c04273a8e70aee
이경민 <lienero@naver.com>
2021-11-16 19:27:29 +0900

    설정파일 변경(시간,DB)

--- ch3/mysite/settings.py
+++ ch3/mysite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3309'
     }
 }
 
@@ -105,7 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
