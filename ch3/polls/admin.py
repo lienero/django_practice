9d206dce99cddab42ef99f7d665d5b04f06088d2
이경민 <lienero@naver.com>
2021-11-22 19:03:42 +0900

    admin 사이트에 테이블 반영

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.db.models.query_utils import Q
+# models 파일에서 정의한 테이블로 Admin 사이트에 보이도록 등록한다.
+from polls.models import Question, Choice
 
 # Register your models here.
+# admin.site.register() 함수를 사용하여 임포트한 클래스를 Admin 사이트에 등록해주면 됩니다.
+admin.site.register(Question)
+admin.site.register(Choice)
