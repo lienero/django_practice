05ca6efdd143d7ae0c707fc28c3b5297d60bda48
이경민 <lienero@naver.com>
2021-12-21 19:00:27 +0900

    admin 페이지 수정

--- ch3/polls/admin.py
+++ ch3/polls/admin.py
@@ -3,7 +3,15 @@ from django.db.models.query_utils import Q
 # models 파일에서 정의한 테이블로 Admin 사이트에 보이도록 등록한다.
 from polls.models import Question, Choice
 
+# ModelAdmin을 상속
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # 검색 박스 추가
+    search_fields = ['question_text']
+
+
 # Register your models here.
 # admin.site.register() 함수를 사용하여 임포트한 클래스를 Admin 사이트에 등록해주면 됩니다.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
