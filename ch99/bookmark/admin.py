14b3393c1c2607ffaa2fd5bec1f89bafdcae02dd
이경민 <lienero@naver.com>
2022-06-27 16:40:09 +0900

    admin 에 북마크 테이블 관련 설정 추가,  마이그레이션

--- ch99/bookmark/admin.py
+++ ch99/bookmark/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
+
+# 테이블을 새로 만들 때에는 models.py와 admin.py 두개의 파일을 함꼐 수정해야한다.
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'url')
 # Register your models here.
