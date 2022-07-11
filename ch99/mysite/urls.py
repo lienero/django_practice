b88c41a0bbbddca55f061512a5536e89e631ce76
イ・ギョンミン <g.i@fragor.jp>
2022-07-11 18:23:53 +0900

    버그수정

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     # path 함수는 route, view 2개의 필수 인자와 kwargs, name 2개의 선택 인자를 받습니다.
     # 여기서 정해준 name 인자값은 템플릿 파일에서 많이 사용됩니다.
     # MVT 원칙을 따르기 위해서 뷰로직을 URL에서 적용하지 않는다.
-    path('boookmark/', BookmarkLV.as_view(), name='index'),
+    path('bookmark/', BookmarkLV.as_view(), name='index'),
     path('bookmark/<int:pk>', BookmarkDV.as_view(), name='detail')
 ]
