12cd797f43378434285d0bf7b58cf3593db053e4
イ・ギョンミン <g.i@fragor.jp>
2022-07-12 18:45:33 +0900

    blog 뷰작성

--- ch99/blog/views.py
+++ ch99/blog/views.py
@@ -1,3 +1,48 @@
+import imp
+from operator import imod
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    # 별도로 지정하더라도 디폴트 컨텍스트 변수명인 'object_list'도 사용할 수 있다.
+    context_object_name = 'posts'
+    # 페이지 기능 사용
+    paginate_by = 2
+
+
+class PostDV(DetailView):
+    model = Post
+
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    # 해당 연도에 해당하는 객체의 리스트를 만들어서 템플릿에 넘겨줌. 'object_list'도 사용할 수 있다.
+    make_object_list = True
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
