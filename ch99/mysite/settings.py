25bf188a306fc81e217773657e3cc8a58e5ac7a5
이경민 <lienero@naver.com>
2022-07-22 18:20:27 +0900

    Photo 앱 생성 및 모델(admin포함)작성

--- ch99/mysite/settings.py
+++ ch99/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bookmark.apps.BookmarkConfig',
     'blog.apps.BlogConfig',
+    'photo.apps.PhotoConfig',
     'taggit.apps.TaggitAppConfig',
     'taggit_templatetags2',
 ]
