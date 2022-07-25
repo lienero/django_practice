537f5c6b36cbdd33fde4cd68202245890bfe5805
이경민 <lienero@naver.com>
2022-07-25 17:06:14 +0900

    유저 인증 기능 설정 및 view, url 코딩

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -1,19 +1,23 @@
+import django
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from mysite.views import HomeView
+from mysite.views import HomeView, UserCreateView, UserCreateDoneTV
 
 # from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done/',
+         UserCreateDoneTV.as_view(), name='register_done'),
     path('', HomeView.as_view(), name='home'),
     # 처리를 bookmark url로 위임
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     path('photo/', include('photo.urls')),
-
     # class-based views
     # path 함수는 route, view 2개의 필수 인자와 kwargs, name 2개의 선택 인자를 받습니다.
     # 여기서 정해준 name 인자값은 템플릿 파일에서 많이 사용됩니다.
--- ch99/mysite/views.py
+++ ch99/mysite/views.py
@@ -1,5 +1,23 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+# reverse_lazy(), reverse() 함수는 인자로 URL 패턴명을 받습니다.
+# URL 패턴명을 인식하기 위해서는 url.py 모듈이 메모리에 로딩되어야 하나,
+# 지금 작성하는 view.py 모듈이 로딩되고 처리되는 시점에 urls.py 모듈이 로딩되지 않을 수 있어 reverse_lazy() 함수를 임포트
+from django.urls import reverse_lazy
 
 
+# 홈페이지 뷰
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+# 로그인 관련 뷰
+class UserCreateView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    # 폼에 임력된 내용에 에러가 없고 테이블 레코드 생성이 완료된 후에 이동할 URL을 지정합니다.
+    success_url = reverse_lazy('register_done')
+
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registration/register_done.html'
