13de4b037f0be0b67eadf6e1a79336ccf9a18736
이경민 <lienero@naver.com>
2022-07-26 17:41:12 +0900

    url 작성

--- ch99/bookmark/urls.py
+++ ch99/bookmark/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from bookmark.views import BookmarkLV, BookmarkDV
+# from bookmark.views import BookmarkLV, BookmarkDV
+from bookmark import views
 
 # url 패턴의 이름을 정해 url 패턴 이름이 충돌되지 않도록 한다.
 app_name = 'bookmark'
 urlpatterns = [
     # 패턴 이름은 bookmark:index가 된다.
-    path('', BookmarkLV.as_view(), name='index'),
-    path('<int:pk>', BookmarkDV.as_view(), name='detail'),
+    path('', views.BookmarkLV.as_view(), name='index'),
+    path('<int:pk>', views.BookmarkDV.as_view(), name='detail'),
+    path('add/', views.BookmarkCreateView.as_view(), name='add',),
+    path('change/', views.BookmarkChangeLV.as_view(), name='change',),
+    path('<int:pk>/update/', views.BookmarkUpdateView.as_view(), name='update',),
+    path('<int:pk>/delete/', views.BookmarkDeleteView.as_view(), name='delete',)
 ]
